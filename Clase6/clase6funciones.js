f6a8949404bc1eeca5f2c3901b852311f09f250f
NathyPaillamilla <npaillamilla@yahoo.cl>
2024-11-16 11:56:01 -0300

    Clase6

---
+++ Clase6/clase6funciones.js
@@ -0,0 +1,28 @@
+const numArry = (a,b,c) => {
+    const res = (x, y, z) => x - y - z
+}
+
+const prom = (a, b, c) => {
+    const res = resta (a, c, c)
+    return res /3
+}
+
+let nombre = ""
+
+nombre = "Andres"
+
+const prom2 = function (a, b, c) {
+    const res = resta (a, c, c)
+    return res /3
+}
+
+//console.log(prom(50,30,10))
+
+
+
+
+
+
+
+
+
